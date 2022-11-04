b7f638105028f5ee5c8373b72e3b7cf94b61b4d3
Yasinsioo <39727640+yasinsioo@users.noreply.github.com>
2022-11-04 12:37:18 +0300

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Employee myObj = new Employee("Yasin", "Alabay", 1000,6,2);
+        myObj.displayDetails();
+    }
+}
